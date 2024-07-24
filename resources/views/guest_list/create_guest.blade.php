6177829651794aac51b58ad89102366fb02e763c
Santosh kumar <lucky1999mfp@gmail.com>
2024-07-24 21:08:17 +0530

    Alter new guest-list form as per the decided layout Fixes #9

--- resources/views/guest_list/create_guest.blade.php
+++ resources/views/guest_list/create_guest.blade.php
@@ -4,20 +4,20 @@
         </x-slot>
         <div class="container ">
             
-            <div class="row justify-content-center mt-5">
-                <div class="col-6 border border-5">
+            <!-- <div class="row justify-content-center mt-5">
+                <div class="col-6 border border-5"> -->
                 <h1>Guest list</h1>
                     <form action="{{route('store.guest')}}" method="post">
                         @csrf
                         <div class="form-group">
-                            <label for="">Name</label>
-                            <input type="text" name="name" class="form-control">
+                            <label for="" class="form-label">Name</label>
+                            <input type="text" name="name" class="form-control mb-3">
                         </div>
                         <div class="form-group">
-                            <label for="">Discription</label>
-                            <textarea name="discription" id="" class="form-control"> </textarea>
+                            <label for="" class="form-label">Description</label>
+                            <textarea name="discription" id="" class="form-control mb-3"> </textarea>
                         </div>
-                        <button type="submit" class="btn btn-primary m-2">Submit</button>
+                        <button type="submit" class="btn btn-primary ">Submit</button>
                     </form>
                 </div>
             </div>
