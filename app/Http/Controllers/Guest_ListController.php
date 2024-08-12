10c68bfd83954f38ed6252038746633667d25095
Santosh kumar <lucky1999mfp@gmail.com>
2024-08-12 18:09:00 +0530

    completed crud operation

--- app/Http/Controllers/Guest_ListController.php
+++ app/Http/Controllers/Guest_ListController.php
@@ -27,5 +27,37 @@ class Guest_ListController extends Controller
         $guest_list->save();
         return redirect()->route('guest_list.all');
     }
+
+    public function show($id)
+    {
+        $guest=GuestList::find($id);
+        
+         return view('guest_list/guest_details')->with('guest',$guest);
+    }
+
+    public function edit($id)
+    {
+        $guest=GuestList::find($id);
+        return view('guest_list/guest_edit')->with('guest', $guest);
+    }
+
+    public function update(Request $request)
+    {
+        
+        $guest_list = GuestList::find($request->id);
+        $guest_list->name=$request->name;
+        $guest_list->discription=$request->discription;
+            // dd($guest_list);
+        
+        $guest_list->save();
+        return redirect()->route('guest_list.all');
+    }
+
+    public function delete($id)
+    {
+        $guest=GuestList::find($id);
+        $guest->delete();
+        return redirect()->route('guest_list.all');
+    }
 }
 
--- resources/views/guest_list/guest_list.blade.php
+++ resources/views/guest_list/guest_list.blade.php
@@ -1,30 +1,24 @@
-
 <x-layout>
-    <x-slot:title>
-        Guest List   
-        </x-slot>
-        <div class="alert alert-light">
-            <a href="/create_guest">Create a new Guest list</a>
-        </div>
-        <div class="container">
-            <h2>All guest Lists</h2>
-            <div class="row">
-            @foreach($guests as $guest)
-                <div class="col-2">
-                    <div class="card"style="hight: 18rem;">
-                        <div class="card-body">
-                             {{$guest->discription}}
-                        </div>
-                        <div class="card-footer">
-                            {{$guest->name}}
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-                
-                    
-               
-            
-        </div>
+  <x-slot:title>
+    Guest List
+    </x-slot>
+    <div class="alert alert-light">
+      <a href="/create_guest">Create a new Guest list</a>
+    </div>
+
+      <h2>All guest Lists</h2>
+        <table class="table">
+          <tr>
+            <th>name</th>
+            <th>discription</th>
+          </tr>
+          @foreach($guests as $guest)
+          <tr>
+            <td>{{$guest->name}}</td>
+            <td>{{$guest->discription}}</td>
+            <td><a href="{{route('guest_details',$guest->id)}}"class="btn btn-primary">Details</a></td>
+          </tr>
+          @endforeach
+        </table>
+ 
 </x-layout>
